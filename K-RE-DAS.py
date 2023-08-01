dd85b37452bdd2b8c5259c89c7f52484520df814
Gilforge <gilles.forge@gmail.com>
2023-08-01 03:26:16 +0200

    Titre + cadre

--- K-RE-DAS.py
+++ K-RE-DAS.py
@@ -4,18 +4,25 @@ import random
 # Initialiser Pygame
 pygame.init()
 
-# Charger les images
-pique = pygame.image.load('img/pique.png')
-carreau = pygame.image.load('img/carreau.png')
-coeur = pygame.image.load('img/coeur.png')
-trefle = pygame.image.load('img/trefle.png')
+# Définir les variables du jeu
+# set the pygame window name
+pygame.display.set_caption('K-RE-DAS')
+game_title = "K-RE-DAS"
+game_author = "by Gilles Aubin"
+score = 0
+margin = 50  # Espace pour le titre et le score
+window_size = (600, 600)  # Taille de la fenêtre
+cell_size = (window_size[0] - 2 * margin) // 10  # Taille de chaque cellule de la grille
 
-# Stocker les images dans un dictionnaire pour un accès facile
+# Créer la fenêtre
+window = pygame.display.set_mode(window_size)
+
+# Charger les images
 symbols = {
-    "pique": pique,
-    "carreau": carreau,
-    "coeur": coeur,
-    "trefle": trefle
+    "pique": pygame.image.load('img/pique.png'),
+    "carreau": pygame.image.load('img/carreau.png'),
+    "coeur": pygame.image.load('img/coeur.png'),
+    "trefle": pygame.image.load('img/trefle.png')
 }
 
 # Créer une grille vide de 10x10
@@ -23,25 +30,8 @@ grid = []
 for i in range(10):
     row = []
     for j in range(10):
-        row.append(None)  # None représente une cellule vide
+        row.append(random.choice(list(symbols.keys())))  # Remplir la grille avec des symboles aléatoires
     grid.append(row)
-
-# Remplir la grille avec des symboles aléatoires
-for i in range(10):
-    for j in range(10):
-        grid[i][j] = random.choice(list(symbols.keys()))
-
-# Définir la taille de la fenêtre
-window_size = (400, 400)
-
-# Créer la fenêtre
-window = pygame.display.set_mode(window_size)
-
-# Définir une couleur pour le fond (RGB)
-background_color = (200, 200, 200)  # Gris
-
-# Taille de chaque cellule de la grille
-cell_size = 40
 
 # Variable pour suivre le symbole actuellement sélectionné et sa position
 selected_symbol = None
@@ -50,6 +40,40 @@ selected_pos = None
 # Boucle principale du jeu
 running = True
 while running:
+    # Remplir l'écran avec la couleur de fond
+    window.fill((200, 200, 200))  # Gris
+
+    # Dessiner le titre
+    fontTitle = pygame.font.Font("font/JetBrainsMono-ExtraBold.ttf", 36)
+    fontScore = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 24)
+    fontAuthor = pygame.font.Font("font/JetBrainsMono-Light.ttf", 12)
+    textTitle = fontTitle.render(game_title, True, (0, 0, 0))
+    textAuthor = fontAuthor.render(game_author, True, (0, 0, 0))
+    window.blit(textTitle, (window_size[0] // 2 - textTitle.get_width() // 2, margin // 2 - textTitle.get_height() // 2))
+    window.blit(textAuthor, (window_size[0] // 2 - textAuthor.get_width() // 2, margin // 2 - textAuthor.get_height() // 2))
+
+    # Dessiner le score
+    textScore = fontScore.render("Score: " + str(score), True, (0, 0, 0))
+    window.blit(textScore, (window_size[0] // 2 - textScore.get_width() // 2, window_size[1] - margin // 2 - textScore.get_height() // 2))
+
+    # Dessiner le cadre
+    pygame.draw.rect(window, (0, 0, 0), (margin, margin, window_size[0] - 2 * margin, window_size[1] - 2 * margin), 2)
+
+    # Dessiner la grille
+    for i in range(10):
+        for j in range(10):
+            symbol = grid[i][j]
+            if symbol is not None:
+                pos = (margin + j * cell_size, margin + i * cell_size)
+                window.blit(symbols[symbol], pos)
+
+    # Si un symbole est sélectionné, le dessiner à la position de la souris
+    if selected_symbol is not None:
+        window.blit(symbols[selected_symbol], (selected_pos[0] - cell_size // 2, selected_pos[1] - cell_size // 2))
+
+    # Mettre à jour l'affichage
+    pygame.display.flip()
+
     # Parcourir tous les événements
     for event in pygame.event.get():
         # Si l'événement est le fait de fermer la fenêtre, arrêter la boucle
@@ -60,48 +84,25 @@ while running:
             # Obtenir la position de la souris
             mouse_pos = pygame.mouse.get_pos()
             # Calculer l'indice de la cellule
-            cell_x = mouse_pos[0] // cell_size
-            cell_y = mouse_pos[1] // cell_size
+            cell_x = (mouse_pos[0] - margin) // cell_size
+            cell_y = (mouse_pos[1] - margin) // cell_size
             # Sélectionner le symbole et sa position
             selected_symbol = grid[cell_y][cell_x]
             selected_pos = (cell_x, cell_y)
-        # Si l'événement est un mouvement de souris
-        elif event.type == pygame.MOUSEMOTION:
-            # Si un symbole est sélectionné
-            if selected_symbol is not None:
-                # Déplacer le symbole à la position de la souris
-                selected_pos = event.pos
         # Si l'événement est le relâchement du bouton de la souris
         elif event.type == pygame.MOUSEBUTTONUP:
             # Si un symbole est sélectionné
             if selected_symbol is not None:
                 # Calculer l'indice de la cellule la plus proche
-                cell_x = min(max(round((selected_pos[0] + cell_size // 2) / cell_size), 0), 9)
-                cell_y = min(max(round((selected_pos[1] + cell_size // 2) / cell_size), 0), 9)
+                cell_x = min(max(round((event.pos[0] + cell_size // 2) / cell_size), 0), 9)
+                cell_y = min(max(round((event.pos[1] + cell_size // 2) / cell_size), 0), 9)
                 # Échanger le symbole sélectionné avec le symbole de la cellule la plus proche
-                grid[selected_pos[1]//cell_size][selected_pos[0]//cell_size], grid[cell_y][cell_x] = grid[cell_y][cell_x], grid[selected_pos[1]//cell_size][selected_pos[0]//cell_size]
+                temp = grid[selected_pos[1]//cell_size][selected_pos[0]//cell_size]
+                grid[selected_pos[1]//cell_size][selected_pos[0]//cell_size] = grid[cell_y][cell_x]
+                grid[cell_y][cell_x] = temp
                 # Réinitialiser le symbole sélectionné et sa position
                 selected_symbol = None
                 selected_pos = None
-
-    # Remplir l'écran avec la couleur de fond
-    window.fill(background_color)
-
-    # Dessiner la grille
-    for i in range(10):
-        for j in range(10):
-            symbol = grid[i][j]
-            if symbol is not None:
-                # Utiliser la position de la grille de positions
-                pos = (j * cell_size, i * cell_size)
-                window.blit(symbols[symbol], pos)
-
-    # Si un symbole est sélectionné, le dessiner à la position de la souris
-    if selected_symbol is not None:
-        window.blit(symbols[selected_symbol], (selected_pos[0] - cell_size // 2, selected_pos[1] - cell_size // 2))
-
-    # Mettre à jour l'affichage
-    pygame.display.flip()
 
 # Quitter Pygame une fois la boucle terminée
 pygame.quit()
