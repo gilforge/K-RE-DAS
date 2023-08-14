77f154185f1752a755b7c93d69cfb78942e8ada1
Gilforge <gilles.forge@gmail.com>
2023-08-14 15:33:23 +0200

    SplashScreen and Help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+splash_image = pygame.image.load('img/splash_windows.png')  # Assurez-vous que l'image est dans le bon répertoire
+
 # Initialiser Pygame
 pygame.init()
 
@@ -120,55 +122,6 @@ def draw_grid():
                 if (i, j) in to_delete:
                     pygame.draw.rect(window, (255, 255, 255), (pos[0], pos[1], cell_size, cell_size), 3)
 
-###########
-# BOUTONS #
-###########
-
-# Dessiner le bouton Nouveau
-width_new = 200
-height_new = 40
-posX_new = 50
-posY_new = 590
-posX_text_new = posX_new + 10
-posY_text_new = posY_new + 5
-
-# Dessiner le bouton Quitter
-width_quit = 200
-height_quit = 40
-posX_quit = 350
-posY_quit = 590
-posX_text_quit = posX_quit + 10
-posY_text_quit = posY_quit + 5
-
-def draw_button_new():
-    pygame.draw.rect(window, color_White, (posX_new, posY_new, width_new, height_new),1)
-    font_new = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 20)
-    text_new = font_new.render("Nouveau", True, color_White) 
-    window.blit(text_new, (posX_text_new, posY_text_new)) 
-
-def draw_button_quit():
-    pygame.draw.rect(window, color_White, (posX_quit, posY_quit, width_quit, height_quit),1)
-    font_quit = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 20)
-    text_quit = font_quit.render("Quitter", True, color_White) 
-    window.blit(text_quit, (posX_text_quit, posY_text_quit)) 
-
-# Détecter le clic sur le high score
-def click_on_button_HS(mouse_pos):
-    if textHighScore_X - 10 < mouse_pos[0] < (textHighScore_X - 10 + 100) and textHighScore_Y < mouse_pos[1] < (textHighScore_Y + 40): 
-        return True
-    return False
-
-# Détecter le clic sur le bouton Nouveau
-def click_on_button_new(mouse_pos):
-    if posX_new < mouse_pos[0] < (posX_new + width_new) and posY_new < mouse_pos[1] < (posY_new + height_new): 
-        return True
-    return False
-
-# Détecter le clic sur le bouton Nouveau
-def click_on_button_quit(mouse_pos):
-    if posX_quit < mouse_pos[0] < (posX_quit + width_quit) and posY_quit < mouse_pos[1] < (posY_quit + height_quit): 
-        return True
-    return False
 
 #########
 # SCORE #
@@ -192,6 +145,158 @@ def reset_game():
         grid.append(row)
     score = 0
 
+
+###########
+# BOUTONS #
+###########
+
+
+# >>>>>> BOUTON RESET HIGH SCORE
+
+# Détecter le clic sur le high score
+def click_on_button_HS(mouse_pos):
+    if textHighScore_X - 10 < mouse_pos[0] < (textHighScore_X - 10 + 100) and textHighScore_Y < mouse_pos[1] < (textHighScore_Y + 40): 
+        return True
+    return False
+
+
+# >>>>>> BOUTON NOUVEAU
+
+# Variables du bouton NOUVEAU
+width_new = 152
+height_new = 40
+posX_new = 58
+posY_new = 590
+posX_text_new = posX_new + 10
+posY_text_new = posY_new + 5
+
+def draw_button_new():
+    pygame.draw.rect(window, color_White, (posX_new, posY_new, width_new, height_new),1)
+    font_new = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 20)
+    text_new = font_new.render("Nouveau", True, color_White) 
+    window.blit(text_new, (posX_text_new, posY_text_new)) 
+
+# Détecter le clic sur le bouton Nouveau
+def click_on_button_new(mouse_pos):
+    if posX_new < mouse_pos[0] < (posX_new + width_new) and posY_new < mouse_pos[1] < (posY_new + height_new): 
+        return True
+    return False
+
+
+# >>>>>> BOUTON AIDE
+
+# Variables du bouton AIDE
+width_help = 152
+height_help = 40
+posX_help = posX_new + width_new + 10
+posY_help = 590
+posX_text_help = posX_help + 10
+posY_text_help = posY_help + 5
+
+def draw_button_help():
+    pygame.draw.rect(window, color_White, (posX_help, posY_help, width_help, height_help), 1)
+    font_help = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 20)
+    text_help = font_help.render("Aide", True, color_White)
+    window.blit(text_help, (posX_text_help, posY_text_help))
+
+# Détecter le clic sur le bouton Aide
+def click_on_button_help(mouse_pos):
+    if posX_help < mouse_pos[0] < (posX_help + width_help) and posY_help < mouse_pos[1] < (posY_help + height_help):
+        print(f"{showing_help} | BOUTON AIDE")
+        return True
+    return False
+
+
+# >>>>>> BOUTON QUITTER
+
+# Variables du bouton QUITTER
+width_quit = 152
+height_quit = 40
+posX_quit = posX_help + width_help + 10
+posY_quit = 590
+posX_text_quit = posX_quit + 10
+posY_text_quit = posY_quit + 5
+
+def draw_button_quit():
+    pygame.draw.rect(window, color_White, (posX_quit, posY_quit, width_quit, height_quit),1)
+    font_quit = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 20)
+    text_quit = font_quit.render("Quitter", True, color_White) 
+    window.blit(text_quit, (posX_text_quit, posY_text_quit)) 
+
+# Détecter le clic sur le bouton Nouveau
+def click_on_button_quit(mouse_pos):
+    if posX_quit < mouse_pos[0] < (posX_quit + width_quit) and posY_quit < mouse_pos[1] < (posY_quit + height_quit): 
+        return True
+    return False
+
+
+# >>>>>> BOUTON FERMER
+
+# Variables du bouton FERMER
+width_close = 152
+height_close = 40
+posX_close = posX_new + width_new + 10
+posY_close = 590
+posX_text_close = posX_close + 10
+posY_text_close = posY_close + 5
+
+# Dessiner le bouton Fermer
+def draw_button_close():
+    pygame.draw.rect(window, color_White, (posX_close, posY_close, width_close, height_close), 1)
+    font_close = pygame.font.Font("font/JetBrainsMono-Bold.ttf", 20)
+    text_close = font_close.render("Fermer", True, color_White)
+    window.blit(text_close, (posX_text_close, posY_text_close))
+
+# Détecter le clic sur le bouton Fermer
+def click_on_button_close(mouse_pos):
+    if posX_close < mouse_pos[0] < (posX_close + width_close) and posY_close < mouse_pos[1] < (posY_close + height_close):
+        return True
+    return False
+
+
+##############
+# ECRAN AIDE #
+##############
+
+# Afficher l'écran d'aide
+def show_help_screen():
+    help_image = pygame.image.load('img/aide.jpg')
+    window.blit(help_image, (0, 0))
+    draw_button_close()
+    pygame.display.flip()
+
+
+#################
+# SPLASH SCREEN #
+#################
+
+# Transition entre 2 images
+def fade_between_images(image1, image2):
+    for i in range(256):
+        alpha = i  # Alpha pour l'image1
+        beta = 255 - i  # Alpha pour l'image2
+
+        # Créez une copie temporaire des images avec les valeurs alpha appropriées
+        splash_temp = image2.copy()
+        game_temp = image1.copy()
+        splash_temp.set_alpha(alpha)
+        game_temp.set_alpha(beta)
+
+        # Dessinez les images avec les valeurs alpha
+        window.blit(splash_temp, (0, 0))
+        window.blit(game_temp, (0, 0))
+
+        # Mettez à jour l'affichage
+        pygame.display.flip()
+
+        # Attendez un peu pour voir l'effet de fondu
+        pygame.time.wait(10)  # Attendre 10 millisecondes
+
+# Utilisation de la fonction
+splash_image = pygame.image.load('img/splash_windows.png')
+game_image = pygame.image.load('img/tapis.jpg')
+fade_between_images(splash_image, game_image)
+
 # Variable pour suivre le symbole actuellement sélectionné et sa position
 selected_symbol1 = None
 selected_pos1 = None
@@ -204,7 +309,25 @@ selected_pos2 = None
 #####################
 
 running = True
+showing_help = False
+
 while running:
+    if showing_help:
+        show_help_screen()
+        pygame.display.flip()
+
+        # Boucle interne pour attendre un clic de souris
+        while showing_help:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    showing_help = False
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if click_on_button_close(mouse_pos):
+                        showing_help = False
+
+        continue  # Continue à la prochaine itération de la boucle, en sautant le reste du code
 
     # Remplir l'écran avec la couleur de fond
     image_fond = pygame.image.load("img/tapis.jpg")
@@ -235,6 +358,7 @@ while running:
     window.blit(textScore, (450,15))
 
     draw_button_new()
+    draw_button_help()
     draw_button_quit()
 
     to_delete = []
@@ -266,19 +390,29 @@ while running:
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_pos = pygame.mouse.get_pos()
 
+            if showing_help:
+                # Gestion des clics sur l'écran d'aide
+                if click_on_button_close(mouse_pos):
+                    showing_help = False
+                continue  # Continue à la prochaine itération pour éviter de traiter d'autres clics
+
+            # Gestion des clics sur l'écran de jeu principal
             if click_on_button_new(mouse_pos):
                 high_score = update_high_score(score, high_score)
                 reset_game()
-                print(f"NOUVEAU")
 
-            if click_on_button_quit(mouse_pos):
+            elif click_on_button_quit(mouse_pos):
                 high_score = update_high_score(score, high_score)
                 running = False
+
+            elif click_on_button_help(mouse_pos):
+                showing_help = True
+                show_help_screen()
 
             if click_on_button_HS(mouse_pos):
                 high_score = 0
                 save_high_score(high_score)
-                print(f"RESET HS")
+                # print(f"RESET HS")
 
             cell_x = (mouse_pos[0] - grid_x) // cell_size
             cell_y = (mouse_pos[1] - grid_y) // cell_size
